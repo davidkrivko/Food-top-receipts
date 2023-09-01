968b6b37d2675d9d4ab924a192a84e7e9a597bb0
Davyd <davidkrivko@gmail.com>
2023-09-01 15:23:07 +0200

    file_path: file name optional

--- restaurants/file_path.py
+++ restaurants/file_path.py
@@ -1,5 +1,5 @@
 import os
 
 
-def get_pdf_file_path(instance, filename):
+def get_pdf_file_path(instance, filename=None):
     return os.path.join("pdf", f"{instance.order.id}_{instance.get_type_display()}.pdf")
